e00062c62a4ac232a713a5306872be5745b242ed
Mahesh Thiruvasagam <mahi09042006@gmail.com>
2025-08-27 10:46:05 +0530

    Update App component to include OurFocus, Impact, and Involved

--- NGO/src/App.jsx
+++ NGO/src/App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import Nav from "./components/nav";
 import Home from "./components/Home";
 import Aboutus from "./components/Aboutus";
+import OurFocus from "./components/OurFocus";
+import Impact from "./components/Impact";
+import Involved from "./components/Involved";
 
 function App() {
   return (
@@ -9,10 +12,11 @@ function App() {
       <Nav />
       
       <main>
-  <Home />
+        <Home />
         <Aboutus />
-        
-       
+        <OurFocus />
+        <Impact />
+        <Involved />
       </main>
     </div>
   );
